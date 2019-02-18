a12f9470cf2431b8a3d7101087b498ab32832fde
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2019-02-18 20:20:31 +0800

    add comments
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- tikv/raftstore/util.go
+++ tikv/raftstore/util.go
@@ -8,12 +8,52 @@ import (
 type LeaseState int
 
 const (
+	/// The lease is suspicious, may be invalid.
 	LeaseState_Suspect LeaseState = 1
+	/// The lease is valid.
 	LeaseState_Valid
+	/// The lease is expired.
 	LeaseState_Expired
 )
 
+/// Lease records an expired time, for examining the current moment is in lease or not.
+/// It's dedicated to the Raft leader lease mechanism, contains either state of
+///   1. Suspect Timestamp
+///      A suspicious leader lease timestamp, which marks the leader may still hold or lose
+///      its lease until the clock time goes over this timestamp.
+///   2. Valid Timestamp
+///      A valid leader lease timestamp, which marks the leader holds the lease for now.
+///      The lease is valid until the clock time goes over this timestamp.
+///
+/// ```text
+/// Time
+/// |---------------------------------->
+///         ^               ^
+///        Now           Suspect TS
+/// State:  |    Suspect    |   Suspect
+///
+/// |---------------------------------->
+///         ^               ^
+///        Now           Valid TS
+/// State:  |     Valid     |   Expired
+/// ```
+///
+/// Note:
+///   - Valid timestamp would increase when raft log entries are applied in current term.
+///   - Suspect timestamp would be set after the message `MsgTimeoutNow` is sent by current peer.
+///     The message `MsgTimeoutNow` starts a leader transfer procedure. During this procedure,
+///     current peer as an old leader may still hold its lease or lose it.
+///     It's possible there is a new leader elected and current peer as an old leader
+///     doesn't step down due to network partition from the new leader. In that case,
+///     current peer lose its leader lease.
+///     Within this suspect leader lease expire time, read requests could not be performed
+///     locally.
+///   - The valid leader lease should be `lease = max_lease - (commit_ts - send_ts)`
+///     And the expired timestamp for that leader lease is `commit_ts + lease`,
+///     which is `send_ts + max_lease` in short.
 type Lease struct {
+	// If boundSuspect is not nil, then boundValid must be nil, if boundValid is not nil, then
+	// boundSuspect must be nil
 	boundSuspect *time.Time
 	boundValid *time.Time
 	maxLease time.Duration
@@ -31,21 +71,27 @@ func NewLease(maxLease time.Duration) *Lease {
 	}
 }
 
+/// The valid leader lease should be `lease = max_lease - (commit_ts - send_ts)`
+/// And the expired timestamp for that leader lease is `commit_ts + lease`,
+/// which is `send_ts + max_lease` in short.
 func (l *Lease) nextExpiredTime(sendTs time.Time) time.Time {
 	return sendTs.Add(l.maxLease)
 }
 
+/// Renew the lease to the bound.
 func (l *Lease) Renew(sendTs time.Time) {
 	bound := l.nextExpiredTime(sendTs)
 	if l.boundSuspect != nil || l.boundValid != nil {
-		if l.boundSuspect != nil && l.boundSuspect < bound {
+		// Longer than suspect ts or longer than valid ts.
+		if l.boundSuspect != nil && l.boundSuspect.Before(bound) {
 			l.boundSuspect = nil
 			l.boundValid = &bound
 		}
-		if l.boundValid != nil && l.boundValid < bound {
+		if l.boundValid != nil && l.boundValid.Before(bound) {
 			l.boundValid = &bound
 		}
 	} else {
+		// Or an empty lease
 		l.boundValid = &sendTs
 	}
 
@@ -60,6 +106,7 @@ func (l *Lease) Renew(sendTs time.Time) {
 	}
 }
 
+/// Suspect the lease to the bound.
 func (l *Lease) Suspect(sendTs time.Time) {
 	l.ExpireRemoteLease()
 	bound := l.nextExpiredTime(sendTs)
@@ -67,6 +114,7 @@ func (l *Lease) Suspect(sendTs time.Time) {
 	l.boundSuspect = &bound
 }
 
+/// Inspect the lease state for the ts or now.
 func (l *Lease) Inspect(ts *time.Time) LeaseState {
 	if l.boundSuspect != nil {
 		return LeaseState_Suspect
@@ -91,17 +139,22 @@ func (l *Lease) Expire() {
 }
 
 func (l *Lease) ExpireRemoteLease() {
+	// Expire remote lease if there is any.
 	if l.remote != nil {
 		l.remote.Expire()
 		l.remote = nil
 	}
 }
 
+/// Return a new `RemoteLease` if there is none.
 func (l *Lease) MaybeNewRemoteLease(term uint64) *RemoteLease {
 	if l.remote != nil {
 		if l.remote.term == term {
+			// At most one connected RemoteLease in the same term.
 			return nil
 		} else {
+			// Term has changed. It is unreachable in the current implementation,
+			// because we expire remote lease when leaders step down.
 			panic("Must expire the old remote lease first!")
 		}
 	}
@@ -113,6 +166,7 @@ func (l *Lease) MaybeNewRemoteLease(term uint64) *RemoteLease {
 		expiredTime: &expiredTime,
 		term: term,
 	}
+	// Clone the remote.
 	remoteClone := &RemoteLease {
 		expiredTime: &expiredTime,
 		term: term,
@@ -121,6 +175,9 @@ func (l *Lease) MaybeNewRemoteLease(term uint64) *RemoteLease {
 	return remoteClone
 }
 
+/// A remote lease, it can only be derived by `Lease`. It will be sent
+/// to the local read thread, so name it remote. If Lease expires, the remote must
+/// expire too.
 type RemoteLease struct {
 	expiredTime *uint64
 	term uint64
