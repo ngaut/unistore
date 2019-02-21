60e57218171fa67111253cc731ef06d1639e59b5
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2019-02-21 11:23:48 +0800

    format code
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- tikv/raftstore/error.go
+++ tikv/raftstore/error.go
@@ -3,14 +3,14 @@ package raftstore
 import (
 	"fmt"
 
-	"github.com/pingcap/kvproto/pkg/metapb"
-	"github.com/pingcap/kvproto/pkg/errorpb"
 	"github.com/pingcap/errors"
+	"github.com/pingcap/kvproto/pkg/errorpb"
+	"github.com/pingcap/kvproto/pkg/metapb"
 )
 
 type ErrNotLeader struct {
 	RegionId uint64
-	Leader *metapb.Peer
+	Leader   *metapb.Peer
 }
 
 func (e *ErrNotLeader) Error() string {
@@ -26,7 +26,7 @@ func (e *ErrRegionNotFound) Error() string {
 }
 
 type ErrKeyNotInRegion struct {
-	Key []byte
+	Key    []byte
 	Region *metapb.Region
 }
 
@@ -44,7 +44,7 @@ func (e *ErrEpochNotMatch) Error() string {
 }
 
 type ErrServerIsBusy struct {
-	Reason string
+	Reason    string
 	BackoffMs uint64
 }
 
@@ -52,7 +52,7 @@ func (e *ErrServerIsBusy) Error() string {
 	return fmt.Sprintf("server is busy, reason %v, backoff ms %v", e.Reason, e.BackoffMs)
 }
 
-type ErrStaleCommand struct {}
+type ErrStaleCommand struct{}
 
 func (e *ErrStaleCommand) Error() string {
 	return fmt.Sprintf("stale command")
@@ -60,7 +60,7 @@ func (e *ErrStaleCommand) Error() string {
 
 type ErrStoreNotMatch struct {
 	RequestStoreId uint64
-	ActualStoreId uint64
+	ActualStoreId  uint64
 }
 
 func (e *ErrStoreNotMatch) Error() string {
@@ -68,7 +68,7 @@ func (e *ErrStoreNotMatch) Error() string {
 }
 
 type ErrRaftEntryTooLarge struct {
-	RegionId uint64
+	RegionId  uint64
 	EntrySize uint64
 }
 
@@ -80,22 +80,22 @@ func RaftstoreErrToPbError(e error) *errorpb.Error {
 	ret := new(errorpb.Error)
 	switch err := errors.Cause(e).(type) {
 	case *ErrNotLeader:
-		ret.NotLeader = &errorpb.NotLeader{RegionId: err.RegionId, Leader: err.Leader }
+		ret.NotLeader = &errorpb.NotLeader{RegionId: err.RegionId, Leader: err.Leader}
 	case *ErrRegionNotFound:
-		ret.RegionNotFound = &errorpb.RegionNotFound{RegionId: err.RegionId }
+		ret.RegionNotFound = &errorpb.RegionNotFound{RegionId: err.RegionId}
 	case *ErrKeyNotInRegion:
 		ret.KeyNotInRegion = &errorpb.KeyNotInRegion{Key: err.Key, RegionId: err.Region.Id,
-			StartKey: err.Region.StartKey, EndKey: err.Region.EndKey }
+			StartKey: err.Region.StartKey, EndKey: err.Region.EndKey}
 	case *ErrEpochNotMatch:
-		ret.EpochNotMatch = &errorpb.EpochNotMatch{ CurrentRegions: err.Regions }
+		ret.EpochNotMatch = &errorpb.EpochNotMatch{CurrentRegions: err.Regions}
 	case *ErrServerIsBusy:
-		ret.ServerIsBusy = &errorpb.ServerIsBusy{Reason: err.Reason, BackoffMs: err.BackoffMs }
+		ret.ServerIsBusy = &errorpb.ServerIsBusy{Reason: err.Reason, BackoffMs: err.BackoffMs}
 	case *ErrStaleCommand:
 		ret.StaleCommand = &errorpb.StaleCommand{}
 	case *ErrStoreNotMatch:
-		ret.StoreNotMatch = &errorpb.StoreNotMatch{RequestStoreId: err.RequestStoreId, ActualStoreId: err.ActualStoreId }
+		ret.StoreNotMatch = &errorpb.StoreNotMatch{RequestStoreId: err.RequestStoreId, ActualStoreId: err.ActualStoreId}
 	case *ErrRaftEntryTooLarge:
-		ret.RaftEntryTooLarge = &errorpb.RaftEntryTooLarge{RegionId: err.RegionId, EntrySize: err.EntrySize }
+		ret.RaftEntryTooLarge = &errorpb.RaftEntryTooLarge{RegionId: err.RegionId, EntrySize: err.EntrySize}
 	default:
 		ret.Message = e.Error()
 	}
