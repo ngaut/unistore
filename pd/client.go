88fe08d5ba1bec46fc0ac02d893c6eb9d41beae2
Ewan Zhou <coocood@gmail.com>
2020-02-15 20:40:36 +0800

    *: add license (#336)

--- pd/client.go
+++ pd/client.go
@@ -1,4 +1,4 @@
-// Copyright 2017 PingCAP, Inc.
+// Copyright 2019-present PingCAP, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -100,7 +100,7 @@ func NewClient(pdAddrs []string, tag string) (Client, error) {
 	log.Infof("[%s][pd] create pd client with endpoints %v", tag, urls)
 
 	c := &client{
-		urls:                     urls,
+		urls: urls,
 		receiveRegionHeartbeatCh: make(chan *pdpb.RegionHeartbeatResponse, 1),
 		checkLeaderCh:            make(chan struct{}, 1),
 		ctx:                      ctx,
