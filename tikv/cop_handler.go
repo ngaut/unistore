05e6598d081ad7b9a2138e9cd0b86a11a3a37eb0
Evan Zhou <coocood@gmail.com>
2020-06-03 20:47:55 +0800

    update tidb version (#398)

--- tikv/cop_handler.go
+++ tikv/cop_handler.go
@@ -177,12 +177,8 @@ func (e *evalContext) newRowDecoder() (*rowcodec.ChunkDecoder, error) {
 		ft := e.fieldTps[i]
 		col := rowcodec.ColInfo{
 			ID:         info.ColumnId,
-			Tp:         int32(ft.Tp),
-			Flag:       int32(ft.Flag),
+			Ft:         ft,
 			IsPKHandle: info.PkHandle,
-			Flen:       ft.Flen,
-			Decimal:    ft.Decimal,
-			Elems:      ft.Elems,
 		}
 		cols = append(cols, col)
 		if info.PkHandle {
