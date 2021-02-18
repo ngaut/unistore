c022a60ccc3fcadb5b3d18093945ea6c9e544628
max <ngaut@126.com>
2021-02-18 15:35:22 +0800

    fix a leak

--- tikv/server.go
+++ tikv/server.go
@@ -688,9 +688,9 @@ func (svr *Server) EstablishMPPConnectionWithStoreId(req *mpp.EstablishMPPConnec
 			return errors.New("tatsk not found")
 		}
 		ctx1, cancel := context.WithCancel(context.Background())
+		defer cancel()
 		tunnel, err := mppHandler.HandleEstablishConn(ctx1, req)
 		if err != nil {
-			cancel()
 			return errors.Trace(err)
 		}
 		var sendError error = nil
@@ -723,6 +723,7 @@ func (svr *Server) EstablishMPPConnectionWithStoreId(req *mpp.EstablishMPPConnec
 func (svr *Server) Raft(stream tikvpb.Tikv_RaftServer) error {
 	return svr.innerServer.Raft(stream)
 }
+
 func (svr *Server) Snapshot(stream tikvpb.Tikv_SnapshotServer) error {
 	return svr.innerServer.Snapshot(stream)
 }
