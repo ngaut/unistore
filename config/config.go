feca5bfd75933b9b652fc4c9fe203133836f2ef9
Meng Huang <791874158@qq.com>
2021-06-21 13:47:44 +0800

    add a concurrency option to s3 (#557)

--- config/config.go
+++ config/config.go
@@ -73,6 +73,7 @@ type S3Options struct {
 	Bucket             string `toml:"bucket"`
 	Region             string `toml:"region"`
 	ExpirationDuration string `toml:"expiration-duration"`
+	Concurrency        int    `toml:"concurrency"`
 }
 
 type PessimisticTxn struct {
